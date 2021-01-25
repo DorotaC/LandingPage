b1aeef8a9a4a7d1889e401722cd443d256f70794
Dorota Delezynska <dorota.delezynska@gmail.com>
2021-01-25 17:56:26 +0100

    For loop added in app.js. CSS not working despite li present.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,36 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
+const navBarList = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('[data-nav]');
+for (let i = 0; i < sections.length; i++){
+  // console.log(sections[i].dataset.nav);
+  const newListItem = document.createElement('li');
+  newListItem.textContent = sections[i].dataset.nav;
+  navBarList.appendChild(newListItem);
+};
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -30,7 +38,7 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
@@ -45,13 +53,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
