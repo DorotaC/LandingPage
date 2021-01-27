481adcfc515a654cdb518de58dc429da51454f46
Dorota Delezynska <dorota.delezynska@gmail.com>
2021-01-27 20:54:41 +0100

    Final v1.0

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 # Landing Page Project
 
-## Table of Contents
+##Project prepared for Udacity FrontEnd Nanodegree.
+_DOM manipulation exercise._
 
-* [Instructions](#instructions)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+Code is based on the template prepared by Udacity team. My input consist of functions
+that create navigation bar (based on contents of index.html file),
+scroll to the chosen section and distinguish visually active section.
--- js/app.js
+++ js/app.js
@@ -12,52 +12,49 @@
  * JS Standard: ESlint
  *
 */
+/*I used information from these Shubham P (mentor) posts on Knowledge:
+https://knowledge.udacity.com/questions/85408#96950
+https://knowledge.udacity.com/questions/66312#66326
 
 /**
  * Define Global Variables
- *
 */
 const navBarList = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('[data-nav]');
-for (let i = 0; i < sections.length; i++){
-  // console.log(sections[i].dataset.nav);
-  const newListItem = document.createElement('li');
-  newListItem.textContent = sections[i].dataset.nav;
-  navBarList.appendChild(newListItem);
+
+//function that creates NavBar
+function createNavBar() {
+  for (let i = 0; i < sections.length; i++) {
+    const newListItem = document.createElement('li');
+    const newAnchorItem = document.createElement('a');
+    newAnchorItem.textContent = sections[i].dataset.nav;
+    newAnchorItem.href = '#section' + (i+1);
+    newAnchorItem.classList.add('section' + (i+1));
+    newListItem.appendChild(newAnchorItem);
+    navBarList.appendChild(newListItem);
+  };
 };
 
+//scrolling to section clicked on NavBar
+function scrollToSections(event) {
+  event.target.scrollIntoView();
+};
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- *
-*/
+//changing class status to active when section is visible
+function activeSection() {
+  for (let i = 0; i < sections.length; i++) {
+    const visibleSection = sections[i].getBoundingClientRect();
+    if (visibleSection.top <= 150 && visibleSection.bottom >= 150){
+      sections[i].classList.add('your-active-class');
+    } else {
+      sections[i].classList.remove('your-active-class');
+    };
+  };
+};
 
 // Build menu
-
+document.addEventListener('DOMContentLoaded', createNavBar);
 // Scroll to section on link click
-
+navBarList.addEventListener('click', scrollToSections);
 // Set sections as active
+document.addEventListener('scroll', activeSection);
